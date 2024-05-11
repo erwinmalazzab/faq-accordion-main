1018653f5b846a541cc152446a0eab0676d6492b
Your Name <erwinmalazzab@gmail.com>
2024-05-11 23:12:28 +0800

    update

---
+++ index.js
@@ -0,0 +1,36 @@
+function click1(){
+  p1.classList.toggle("text-active");
+  if (bt1.includes("./assets/images/icon-minus.svg")) {
+    btn1.setAttribute("src", "./assets/images/icon-plus.svg");
+  } else {
+    btn1.setAttribute("src", "./assets/images/icon-minus.svg");
+  }
+};
+ 
+function click2(){
+  p2.classList.toggle("text-active");
+  if (btn2.includes("./assets/images/icon-minus.svg")) {
+    btn2.setAttribute("src", "./assets/images/icon-plus.svg");
+  } else {
+    btn2.setAttribute("src", "./assets/images/icon-minus.svg");
+  }
+};
+ 
+function click3(){
+  p3.classList.toggle("text-active");
+  if (bt3.includes("./assets/images/icon-minus.svg")) {
+    btn3.setAttribute("src", "./assets/images/icon-plus.svg");
+  } else {
+    btn3.setAttribute("src", "./assets/images/icon-minus.svg");
+  }
+};
+ 
+function click4(){
+  p4.classList.toggle("text-active");
+  if (btn.includes("./assets/images/icon-minus.svg")) {
+    btn4.setAttribute("src", "./assets/images/icon-plus.svg");
+  } else {
+    btn4.setAttribute("src", "./assets/images/icon-minus.svg");
+  }
+};
+ 
