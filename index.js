cbfc7e27b7d5a1e1c95b4c8f7af4972a19ad4959
Your Name <erwinmalazzab@gmail.com>
2024-05-11 23:45:17 +0800

    updated

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 function click1(){
   p1.classList.toggle("text-active");
-  if (bt1.includes("./assets/images/icon-minus.svg")) {
+  p2.classList.remove("text-active");
+  p3.classList.remove("text-active")
+  p4.classList.remove("text-active")
+  btn2.setAttribute("src", "./assets/images/icon-plus.svg");
+  btn3.setAttribute("src", "./assets/images/icon-plus.svg");
+  btn4.setAttribute("src", "./assets/images/icon-plus.svg");
+  if (btn1.getAttribute("src").includes("./assets/images/icon-minus.svg")) {
     btn1.setAttribute("src", "./assets/images/icon-plus.svg");
   } else {
     btn1.setAttribute("src", "./assets/images/icon-minus.svg");
   }
+  
+
 };
  
 function click2(){
   p2.classList.toggle("text-active");
-  if (btn2.includes("./assets/images/icon-minus.svg")) {
+  p1.classList.remove("text-active");
+  p3.classList.remove("text-active")
+  p4.classList.remove("text-active")
+  btn1.setAttribute("src", "./assets/images/icon-plus.svg");
+  btn3.setAttribute("src", "./assets/images/icon-plus.svg");
+  btn4.setAttribute("src", "./assets/images/icon-plus.svg");
+  if (btn2.getAttribute("src").includes("./assets/images/icon-minus.svg")) {
     btn2.setAttribute("src", "./assets/images/icon-plus.svg");
   } else {
     btn2.setAttribute("src", "./assets/images/icon-minus.svg");
@@ -18,7 +32,13 @@ function click2(){
  
 function click3(){
   p3.classList.toggle("text-active");
-  if (bt3.includes("./assets/images/icon-minus.svg")) {
+  p1.classList.remove("text-active");
+  p2.classList.remove("text-active")
+  p4.classList.remove("text-active")
+  btn1.setAttribute("src", "./assets/images/icon-plus.svg");
+  btn2.setAttribute("src", "./assets/images/icon-plus.svg");
+  btn4.setAttribute("src", "./assets/images/icon-plus.svg");
+  if (btn3.getAttribute("src").includes("./assets/images/icon-minus.svg")) {
     btn3.setAttribute("src", "./assets/images/icon-plus.svg");
   } else {
     btn3.setAttribute("src", "./assets/images/icon-minus.svg");
@@ -27,7 +47,13 @@ function click3(){
  
 function click4(){
   p4.classList.toggle("text-active");
-  if (btn.includes("./assets/images/icon-minus.svg")) {
+  p1.classList.remove("text-active");
+  p2.classList.remove("text-active")
+  p3.classList.remove("text-active")
+  btn1.setAttribute("src", "./assets/images/icon-plus.svg");
+  btn2.setAttribute("src", "./assets/images/icon-plus.svg");
+  btn3.setAttribute("src", "./assets/images/icon-plus.svg");
+  if (btn4.getAttribute("src").includes("./assets/images/icon-minus.svg")) {
     btn4.setAttribute("src", "./assets/images/icon-plus.svg");
   } else {
     btn4.setAttribute("src", "./assets/images/icon-minus.svg");
